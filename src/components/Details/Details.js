753b85600ddf2f687de2cd12e10f32786b952915
Adelle Pitsas <apitsas4@gmail.com>
2022-12-03 19:12:14 -0500

    add styling for details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,23 +1,34 @@
 import React from 'react'
+import './Details.css'
 
 const Details = ({ details }) => {
   const styles = {
     backgroundImage: `url(${details['backdrop_path']})`,
-    backgroundRepeat: "no-repeat"
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "750px",
   }
+  // console.log(details.genres)
+  const buttons = details.genres.map(genre => {
+    return <button className='genre'>{genre}</button>
+  })
   return (
     <div style={styles}>
-      <h3>{details.title}</h3>
-      <img name='posterPath' src={details['poster_path']}></img>
-      {/* <img name='backdropPath' src={details['backdrop_path']}></img> */}
-      <p>{details['release_date']}</p>
-      <p>{details.overview}</p>
-      <p>{details['average_rating']}</p>
-      <p>{details.genres}</p>
-      <p>{details.budget}</p>
-      <p>{details.revenue}</p>
-      <p>{details.runtime}</p>
-      <p>{details.tagline}</p>
+      <div className='overlay'></div>
+      <div className='details-container'>
+        <img className='poster-img' name='posterPath' src={details['poster_path']}></img>
+        <div className='details-content'>
+          <h3 className='title'>{details.title}</h3>
+          <h4 className='tagline'>{details.tagline}</h4>
+          <div className='genre-tags'>{buttons}</div>
+          <p>Summary: {details.overview}</p>
+          <p>Release date: {details['release_date']}</p>
+          <p>Rating: {details['average_rating']}</p>
+          <p>Movie Budget: ${details.budget}</p>
+          <p>Box Office Revenue: ${details.revenue}</p>
+          <p>Run time: {details.runtime} mins</p>
+        </div>
+      </div>
     </div>
   )
 }
