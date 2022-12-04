22ca6f5e202eebcad14a0eae190f5dc01d4c5097
Adelle Pitsas <apitsas4@gmail.com>
2022-12-03 21:03:15 -0500

    add error pop up styling

---
+++ src/components/Error/Error.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Error.css'
+import errorIcon from '../../images/alert-icon.png'
+
+const Error = () => {
+  return (
+    <div className="main-overlay">
+      <div className="error">
+        <img src={errorIcon} className="error-icon"></img>
+        <div className="error-content">
+          <h2>Oops! Looks like there as a problem.</h2>
+          <h3>Please try again later.</h3>
+        </div>
+        <button className="dismissButton">Dismiss</button>
+      </div>
+    </div>
+  )
+}
+
+export default Error
