1d21e60a394707813280c9a1c6e0fac4ebd4f8bb
Adelle Pitsas <apitsas4@gmail.com>
2022-12-05 18:59:19 -0500

    add test for displaying movies

--- cypress/e2e/all_movies_spec.cy.js
+++ cypress/e2e/all_movies_spec.cy.js
@@ -9,4 +9,20 @@ describe("All movies page testing", () => {
   it('should display the title of the application', () => {
     cy.contains("Rancid Tomatillos")
   })
+
+  it('should display a collection of movies', () => {
+    cy.get('.MoviesCardsContainer').should('exist')
+    cy.get('.card').should('have.length', 3)
+      .should('be.visible')
+    cy.get('.card').eq(0).should('contain', "Money Plane")
+      .and('contain', 6.88)
+    cy.get('.poster').eq(0).should('have.attr', 'src', "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")
+    cy.get('.card').eq(1).should('contain', "Mulan")
+      .and('contain', 5.10)
+    cy.get('.poster').eq(1).should('have.attr', 'src', "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg")
+    cy.get('.card').eq(2).should('contain', "Rogue")
+      .and('contain', 7.33)
+    cy.get('.poster').eq(2).should('have.attr', 'src', "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg")
+    
+  })
 })
